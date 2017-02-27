d5d2cfee008722ba5826b200b3036e81b1d266fd
Algram <aliasgram@gmail.com>
2017-02-27 17:32:42 +0100

    Updated hyper.js to inlcude new plugins

--- .hyper.js
+++ .hyper.js
@@ -105,7 +105,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyper-statusline', 'hyperterm-chesterish'],
+  plugins: ['hyper-statusline', 'hyperterm-chesterish', 'hyper-dark-scrollbar', 'hypercwd'],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
